15c130b527e5105a944dff0567a2f52ff9fa7590
makbaranov <mursilaga834@yandex.com>
2025-05-24 16:36:38 +0300

    cmakelists and main

---
+++ ZipScout/main.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QCommandLineParser parser;
+    parser.addOption({"test", "Test mod"});
+
+    parser.addOption({"nogui", "Console mod"});
+    parser.addOption({"source", "Sorce file", "path"});
+    parser.addOption({"destination", "Destination file", "path"});
+    parser.addOption({"filter", "Filter, word to search", "string"});
+
+
+    parser.process(a);
+
+    if (parser.isSet("nogui")) {
+        qDebug() << "Console mod";
+
+        if (!parser.isSet("source") || !parser.isSet("destination")) {
+            qCritical() << "Error: --source --destination and --filter must be provided for Console mod";
+            return 1;
+        }
+
+        QString source = parser.value("source");
+        QString dest = parser.value("destination");
+        QString filter = parser.value("filter");
+
+        qDebug() << "Source:" << source;
+        qDebug() << "Destination:" << dest;
+        qDebug() << "Filter:" << filter;
+        return 0;
+    }
+
+    if (parser.isSet("test")) {
+        qDebug() << "Test mod";
+    }
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
